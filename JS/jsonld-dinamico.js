38eaa8e39d4b6f441e50291f4a8ec93aac1434de
Pablo <25508500+PabloMoralesSP@users.noreply.github.com>
2025-10-22 12:25:44 +0200

    Favicon
    Favicon

--- JS/jsonld-dinamico.js
+++ JS/jsonld-dinamico.js
@@ -1,19 +1,25 @@
-
-// jsonld-dinamico.js
+// js/jsonld-dinamico.js
 (function() {
     const jsonLd = {
         "@context": "https://schema.org",
         "@type": "LodgingBusiness",
         "name": "Casa Rural Los Robles",
         "url": window.location.href, // URL de la página actual
-        "image": "http://casarurallosrobles.com/Logo_Los_Robles.png",
-        "logo": "http://casarurallosrobles.com/Logo_Los_Robles.png",,
+        "image": [
+		  "https://casarurallosrobles.com/ImagenesGaleria/Casa/casa_fachada.jpg",
+		  "https://casarurallosrobles.com/ImagenesGaleria/Casa/chimenea.jpg",
+		  "https://casarurallosrobles.com/ImagenesGaleria/Casa/habitacion_principal.jpg"
+		],
+        "logo": "https://casarurallosrobles.com/Logo_Los_Robles.png",
+        "description": "Casa Rural Los Robles, alojamiento rural en Zarzuela de Galve, Guadalajara. Disfruta del encanto de la Sierra Norte, naturaleza y descanso.",
+        "telephone": "+34 678 985 395",
+        "email": "ar.losrobles.valverde@gmail.com",
+		
         "owner": {
             "@type": "Person",
             "name": "Sergio Robledo Monasterio"
         },
-        "telephone": "+34 678 985 395",
-        "email": "ar.losrobles.valverde@gmail.com",
+		
         "address": {
             "@type": "PostalAddress",
             "streetAddress": "Travesía de la Huerta 1",
@@ -22,6 +28,7 @@
             "postalCode": "19238",
             "addressCountry": "ES"
         },
+		
         "geo": {
             "@type": "GeoCoordinates",
             "latitude": 40.595,
